b2ede305f4700363a875ae7e90ab0f9582e1892e
|Azrael| <andriy.simchich@gmail.com>
2019-07-15 17:14:27 +0300

    Delete old dir

--- SPEEDTEST/lng_ukrainian.pl
+++
@@ -1,5 +0,0 @@
-$lang{SPEEDTEST}             = 'Тест швидкості',
-$lang{YOUR_IP}               = 'Ваш IP',
-$lang{BTN_START}             = 'Почати',
-$lang{BTN_STOP}              = 'Зупинити',
-1
--- speedtest_api/getIP.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-print $ENV{REMOTE_ADDR}; #Получение IP
-1;
